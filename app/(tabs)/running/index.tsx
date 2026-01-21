f789788773c63cf546e9e3528c23716d37a12762
DaeHun Kang <crol0101@naver.com>
2026-01-21 19:32:02 +0900

    [✨ Feat] google map뷰 셋팅 (#10)

--- app/(tabs)/running/index.tsx
+++ app/(tabs)/running/index.tsx
@@ -1,12 +1,26 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import React, { useState } from "react";
+import { View } from "react-native";
+import MapView, { PROVIDER_GOOGLE } from "react-native-maps";
 
 const Index = () => {
+  const [coordinates, setCoordinates] = useState({
+    latitude: 37.78825,
+    longitude: -122.4324,
+  });
   return (
-    <View>
-      <Text>index</Text>
+    <View className="flex-1">
+      <MapView
+        style={{ width: "100%", height: "100%" }}
+        initialRegion={{
+          latitude: coordinates.latitude,
+          longitude: coordinates.longitude,
+          latitudeDelta: 0.01,
+          longitudeDelta: 0.01,
+        }}
+        provider={PROVIDER_GOOGLE}
+      ></MapView>
     </View>
-  )
-}
+  );
+};
 
-export default Index
+export default Index;
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,13 +1,16 @@
 import { Stack } from "expo-router";
 import "react-native-reanimated";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
 import { GluestackUIProvider } from "@/components/ui/gluestack-ui-provider";
 import "@/global.css";
 
 export default function RootLayout() {
   return (
-    <GluestackUIProvider mode="dark">
-      <Stack screenOptions={{ headerShown: false }} />
-    </GluestackUIProvider>
+    <SafeAreaProvider>
+      <GluestackUIProvider mode="dark">
+        <Stack screenOptions={{ headerShown: false }} />
+      </GluestackUIProvider>
+    </SafeAreaProvider>
   );
 }
