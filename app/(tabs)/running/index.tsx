41b1a3cae84c9e9909b2d087e8b495d23f553b9b
DaeHun Kang <crol0101@naver.com>
2026-02-14 14:33:33 +0900

    [🐛 Fix:] safearea inset 수정 (#10)

--- app/(tabs)/running/index.tsx
+++ app/(tabs)/running/index.tsx
@@ -4,19 +4,21 @@ import GoogleMap from "@/components/map/GoogleMap";
 import RecRouteSwiper from "@/components/swiper/RecRouteSwiper";
 import React from "react";
 import { View } from "react-native";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 const Index = () => {
   const [isMapLoaded, setIsMapLoaded] = React.useState(false);
+  const insets = useSafeAreaInsets();
 
   return (
-    <>
+    <View className="flex-1 bg-white " style={{ paddingTop: insets.top }}>
       <Header />
       <View className="relative flex-1">
         <RecRouteSwiper disabled={!isMapLoaded} />
         <GoogleMap onMapLoad={() => setIsMapLoaded(true)} />
         <RunStartButton disabled={!isMapLoaded} />
       </View>
-    </>
+    </View>
   );
 };
 
