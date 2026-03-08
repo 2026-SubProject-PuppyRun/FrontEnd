9735c766e55c243d1867c44137faf2a81a776772
DaeHun Kang <crol0101@naver.com>
2026-03-08 13:53:39 +0900

    [✨ Feat] 기본 패딩 적용 (#14)

--- app/(tabs)/guide/_layout.tsx
+++ app/(tabs)/guide/_layout.tsx
@@ -1,2 +1,12 @@
 import { Slot } from "expo-router";
-export default Slot;
+import { View } from "react-native";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+
+export default function HomeLayout() {
+  const insets = useSafeAreaInsets();
+  return (
+    <View style={{ paddingTop: insets.top }} className="flex-1">
+      <Slot />
+    </View>
+  );
+}
