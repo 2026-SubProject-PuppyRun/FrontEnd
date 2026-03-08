c0847cc24dac9d1824836fd10e9980b52aa4deb9
DaeHun Kang <crol0101@naver.com>
2026-03-08 16:52:48 +0900

    [✨ Feat] 홈 탭 기본적인 디자인 추가 (#17)

--- app/(tabs)/home/index.tsx
+++ app/(tabs)/home/index.tsx
@@ -1,12 +1,19 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import HomeAvatar from "@/components/avatar/HomeAvatar";
+import HomeDashBoard from "@/components/board/HomeDashBoard";
+import Header from "@/components/header/Header";
+import React from "react";
+import { Text, View } from "react-native";
 
 const Index = () => {
   return (
     <View>
-      <Text>index</Text>
+      <Header>
+        <Text>로고</Text>
+        <HomeAvatar />
+      </Header>
+      <HomeDashBoard />
     </View>
-  )
-}
+  );
+};
 
-export default Index
+export default Index;
---
+++ components/board/HomeDashBoardItem/WalkScoreBoard.tsx
@@ -0,0 +1,150 @@
+import { Spinner } from "@/components/ui/spinner";
+import { useLocationPermission } from "@/hooks/use-location-permission";
+import { Ionicons } from "@expo/vector-icons";
+import * as Location from "expo-location";
+import React, { useEffect, useRef, useState } from "react";
+import { Text, View } from "react-native";
+
+const WalkScoreBoard = () => {
+  const [coordinates, setCoordinates] = useState({
+    latitude: 37.78825,
+    longitude: -122.4324,
+  });
+  const permission = useLocationPermission();
+  const isLocationInitialized = useRef(false);
+  const [errorMsg, setErrorMsg] = useState<string | null>(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const initLocation = async () => {
+      if (permission === null || isLocationInitialized.current) return;
+      try {
+        if (permission !== true) {
+          if (permission === false) setErrorMsg("위치 권한이 거부되었습니다.");
+          return;
+        }
+
+        const serviceEnabled = await Location.hasServicesEnabledAsync();
+        if (!serviceEnabled) {
+          setErrorMsg("기기 위치 서비스가 꺼져 있습니다. 설정에서 켜 주세요.");
+          return;
+        }
+
+        const location = await Location.getCurrentPositionAsync({
+          accuracy: Location.Accuracy.High,
+        });
+        setCoordinates({
+          latitude: location.coords.latitude,
+          longitude: location.coords.longitude,
+        });
+        isLocationInitialized.current = true;
+      } catch (error) {
+        isLocationInitialized.current = true;
+        console.error("위치 조회 실패:", error);
+        setErrorMsg(
+          "현재 위치를 가져올 수 없습니다. 잠시 후 다시 시도해 주세요.",
+        );
+      } finally {
+        setIsLoading(false);
+        console.log("위치 조회 완료", { coordinates });
+      }
+    };
+    if (permission === true && !isLocationInitialized.current) initLocation();
+  }, [permission]);
+
+  if (isLoading) {
+    return (
+      <View className="flex-1 items-center justify-center">
+        <Spinner size="large" color="#BFB8AA" />
+      </View>
+    );
+  }
+  return (
+    <View className="m-4 gap-6 pb-4">
+      <View className="rounded-lg bg-white p-4 shadow">
+        <View className="flex-row items-center gap-2 self-start rounded-2xl bg-[#737153] px-4 py-2">
+          <Ionicons name="location-sharp" size={20} color="white" />
+          <Text className="text-white">지역</Text>
+        </View>
+      </View>
+      <View className="rounded-lg bg-white px-8 py-4 shadow">
+        <View className="flex-row items-center gap-4">
+          <Ionicons name="cloudy-sharp" size={60} color="#BFB8AA" />
+          <View>
+            <Text>5° 구름많음</Text>
+            <Text>최고 17° 최저 3° </Text>
+          </View>
+        </View>
+        <View className="flex-row justify-between px-4">
+          <View className="items-center gap-1 pt-4">
+            <Text>4시</Text>
+            <Ionicons name="cloudy-sharp" size={40} color="#BFB8AA" />
+            <Text>5°</Text>
+          </View>
+          <View className="items-center gap-1 pt-4">
+            <Text>5시</Text>
+            <Ionicons name="cloudy-sharp" size={40} color="#BFB8AA" />
+            <Text>5°</Text>
+          </View>
+          <View className="items-center gap-1 pt-4">
+            <Text>6시</Text>
+            <Ionicons name="cloudy-sharp" size={40} color="#BFB8AA" />
+            <Text>5°</Text>
+          </View>
+          <View className="items-center gap-1 pt-4">
+            <Text>7시</Text>
+            <Ionicons name="cloudy-sharp" size={40} color="#BFB8AA" />
+            <Text>5°</Text>
+          </View>
+        </View>
+      </View>
+      <View className="gap-4 rounded-lg bg-white p-4 shadow">
+        <View className="flex-row items-center justify-between">
+          <Text>미세먼지</Text>
+          <View className="flex-row items-center gap-1">
+            <Text>좋음</Text>
+            <Ionicons name="happy-sharp" size={24} color="#BFB8AA" />
+          </View>
+        </View>
+        <View className="flex-row items-center justify-between">
+          <Text>초미세먼지</Text>
+          <View className="flex-row items-center gap-1">
+            <Text>좋음</Text>
+            <Ionicons name="happy-sharp" size={24} color="#BFB8AA" />
+          </View>
+        </View>
+      </View>
+      <View className="rounded-lg bg-white p-4 shadow ">
+        <Text>시간대 별 예상 강수량</Text>
+        <View className="flex-row justify-between px-4">
+          <View className="items-center gap-4 pt-4">
+            <Text>4시</Text>
+            <Ionicons name="umbrella-sharp" size={40} color="#BFB8AA" />
+            <Text>10%</Text>
+            <Text>5mm</Text>
+          </View>
+          <View className="items-center gap-4 pt-4">
+            <Text>5시</Text>
+            <Ionicons name="umbrella-sharp" size={40} color="#BFB8AA" />
+            <Text>10%</Text>
+            <Text>5mm</Text>
+          </View>
+          <View className="items-center gap-4 pt-4">
+            <Text>6시</Text>
+            <Ionicons name="umbrella-sharp" size={40} color="#BFB8AA" />
+            <Text>10%</Text>
+            <Text>5mm</Text>
+          </View>
+          <View className="items-center gap-4 pt-4">
+            <Text>7시</Text>
+            <Ionicons name="umbrella-sharp" size={40} color="#BFB8AA" />
+            <Text>10%</Text>
+            <Text>5mm</Text>
+          </View>
+        </View>
+      </View>
+    </View>
+  );
+};
+
+export default WalkScoreBoard;
