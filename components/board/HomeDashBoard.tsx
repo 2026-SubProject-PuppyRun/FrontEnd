b4c990ed73c6c12c5bbcfe12bc85ab0b52edb31f
DaeHun Kang <crol0101@naver.com>
2026-03-09 17:39:41 +0900

    [✨ Feat] 통계 및 알람 라우팅 추가 (#17)

--- components/board/HomeDashBoard.tsx
+++ components/board/HomeDashBoard.tsx
@@ -1,12 +1,36 @@
+import { useRouter } from "expo-router";
 import React from "react";
-import { ScrollView } from "react-native";
+import { ScrollView, View } from "react-native"; // Platform 임포트 추가
+import { Button, ButtonText } from "../ui/button";
 import RunSummaryBoard from "./HomeDashBoardItem/RunSummaryBoard";
 import WalkScoreBoard from "./HomeDashBoardItem/WalkScoreBoard";
 
 const HomeDashBoard = () => {
+  const router = useRouter();
   return (
-    <ScrollView className=" bg-gray-100">
+    <ScrollView className="bg-gray-100">
       <RunSummaryBoard />
+      <View className="flex-row items-center justify-start gap-4 px-4 py-2">
+        <View className="rounded-lg " style={{ elevation: 5 }}>
+          <Button
+            size="xl"
+            className="rounded-lg bg-[#737153] data-[active=true]:bg-[#BFB8AA] "
+            onPress={() => router.push("/(tabs)/home/status")}
+          >
+            <ButtonText className="text-white">통계 보기</ButtonText>
+          </Button>
+        </View>
+
+        <View className="rounded-lg " style={{ elevation: 5 }}>
+          <Button
+            size="xl"
+            className="rounded-lg bg-[#737153] data-[active=true]:bg-[#BFB8AA] "
+            onPress={() => router.push("/(tabs)/home/alarm")}
+          >
+            <ButtonText className="text-white">알람 설정하기</ButtonText>
+          </Button>
+        </View>
+      </View>
       <WalkScoreBoard />
     </ScrollView>
   );
