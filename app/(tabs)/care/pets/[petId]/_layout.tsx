161aa82c4c39ee59da14698c2b1c1d5cf4713edd
DaeHun Kang <crol0101@naver.com>
2026-05-26 19:56:57 +0900

    [📱 Design] 네비게이션 바 임시 디자인 추가 (#22)

---
+++ app/(tabs)/care/pets/[petId]/_layout.tsx
@@ -0,0 +1,15 @@
+import Header from "@/components/header/Header";
+import { Slot } from "expo-router";
+import React from "react";
+import { View } from "react-native";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+
+export default function PetLayout() {
+  const insets = useSafeAreaInsets();
+  return (
+    <View style={{ paddingTop: insets.top }} className="flex-1 bg-white">
+      <Header title="반려견 목록" />
+      <Slot />
+    </View>
+  );
+}
