563d71075856cc14f70fefd80b9f77a82f8d9f90
DaeHun Kang <crol0101@naver.com>
2026-07-27 16:14:33 +0900

    [✨ Feat] 셀피 탭 ui 추가 (#10)

--- app/(tabs)/running/selfie.tsx
+++ app/(tabs)/running/selfie.tsx
@@ -1,19 +1,28 @@
 import RunResultBoard from "@/components/board/RunBoard/RunResultBoard";
 import WriteDiaryButton from "@/components/button/WriteDiaryButton";
-import Header from "@/components/header/Header";
+import RunLogoSvg from "@/components/svg/RunLogoSvg";
 import SelfieAndRouteSwiper from "@/components/swiper/SelfieAndRouteSwiper";
 import useNonNavbar from "@/hooks/use-non-navbar";
 import React from "react";
 import { View } from "react-native";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
+
 const Selfie = () => {
   const insets = useSafeAreaInsets();
+
   useNonNavbar();
+
   return (
-    <View style={{ paddingTop: insets.top }} className="flex-1 bg-white">
-      <Header />
-      <SelfieAndRouteSwiper />
-      <RunResultBoard />
+    <View style={{ paddingTop: insets.top }} className="flex-1 bg-[#F7F7F7]">
+      <View className="items-center pt-3">
+        <RunLogoSvg width={189} height={50} />
+      </View>
+
+      <View className="flex-1 justify-center gap-0">
+        <SelfieAndRouteSwiper />
+        <RunResultBoard />
+      </View>
+
       <WriteDiaryButton />
     </View>
   );
