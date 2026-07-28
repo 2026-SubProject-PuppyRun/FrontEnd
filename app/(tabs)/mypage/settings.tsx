2cfda876bbdfd9c8ae6ee54cf4976cee810492e7
DaeHun Kang <crol0101@naver.com>
2026-07-28 15:07:35 +0900

    [✨ Feat] 세팅 탭 ui 수정 (#21)

--- app/(tabs)/mypage/settings.tsx
+++ app/(tabs)/mypage/settings.tsx
@@ -1,11 +1,10 @@
 import SettingBody from "@/components/body/mypage/SettingBody";
 import Header from "@/components/header/Header";
-import React from "react";
 import { View } from "react-native";
 
 const Settings = () => {
   return (
-    <View className="flex-1">
+    <View className="flex-1 bg-[#F7F7F7]">
       <Header title="설정" />
       <SettingBody />
     </View>
